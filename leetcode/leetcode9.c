eca19473e650f96e728d22ef57cce3aa7329379e
ryan.ren <ryan.ren@siflower.com.cn>
2020-04-15 13:58:52 +0800

    leetcode 9 isPalindrome

--- leetcode/leetcode9.c
+++ leetcode/leetcode9.c
@@ -21,7 +21,7 @@
  *
  * 你能不将整数转为字符串来解决这个问题吗？
  *
- * int isPlaindrome(int x){
+ * bool isPlaindrome(int x){
  * }
  * */
 
@@ -30,6 +30,40 @@
  * 不能转的话就整除取模好了
  * 参考8 得到翻转的数
  * 判断二者是否相等即可
- * 同样 注意溢出嗷
+ * 同样 注意溢出
  * 不过可能又多余了很多计算复杂度！
  * */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+static bool isPalindrome(int x)
+{
+        if (x < 0)
+              return false;
+        else
+        {
+                long int a = x, b = 0;
+                while (a)
+                {
+                        b = 10*b + a%10;
+                        a = a/10;
+                }
+                return (x == b);
+        }
+}
+
+void main(void)
+{
+        if (!isPalindrome(121))
+              printf("fail 121\n");
+
+        if(isPalindrome(-121))
+              printf("fail -121\n");
+
+        if(isPalindrome(10))
+              printf("fail 10\n");
+
+        return;
+
+}
